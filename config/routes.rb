cb987e9eeeeb750d38836c3b5f19f40392760471
Patrick Cronin <pxcronin@gmail.com>
2021-04-28 16:30:45 -0400

    Updated the contact email in the footer and added Devise to authenticate me when viewing the contact results from the contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'contacts#home'
   resources :contacts, only: [:new, :create]
   get '/contacts/portfolio', to: 'contacts#portfolio'
+  get '/contacts/inbox', to: 'contacts#inbox'
 end
